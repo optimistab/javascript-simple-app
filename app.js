eab0083d2662daf697c1143fdb1ed116614f13cd
abhay <abhay.coder@gmail.com>
2020-04-23 11:53:48 +0530

    first stage

---
+++ app.js
@@ -0,0 +1,13 @@
+// function sayHello(name){
+//     console.log('Hello '+name);
+// }
+
+// sayHello('abhay');
+
+// console.log(window);
+// console.log(module);
+var log = require ('./logger');
+
+// console.log(logger);
+log('message'); 
+
---
+++ logger.js
@@ -0,0 +1,13 @@
+var x =;
+var url ='http://mylogger.io/log';
+
+function log(message){
+    console.log(message);
+}
+
+// module.exports.log   = log;
+
+module.exports= log;
+
+
+
